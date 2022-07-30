0eca78381e4100b3a9edc52cf12cd85559132966
Lucas Angelozzi <lucasangelozzi32@gmail.com>
2022-07-30 00:19:53 -0700

    changed url for api

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: true,
-    baseUrl: 'http://recipe-repository-app.herokuapp.com',
-    baseApiUrl: 'http://localhost:3000/api',
+    baseUrl: 'https://recipe-repository-app.herokuapp.com/',
+    baseApiUrl: 'https://recipe-repository-api.herokuapp.com/api',
 };
