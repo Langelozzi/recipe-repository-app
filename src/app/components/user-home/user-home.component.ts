7905d0322d92e413cdd2ad87b1e8e2a9a051bc09
Lucas Angelozzi <lucasangelozzi32@gmail.com>
2022-07-30 13:00:35 -0700

    finished user home component

--- src/app/components/user-home/user-home.component.ts
+++ src/app/components/user-home/user-home.component.ts
@@ -1,24 +1,27 @@
-import { AuthService } from './../../services/auth-service/auth.service';
-import { Component, OnInit, HostListener } from '@angular/core';
-import { Router } from '@angular/router';
-import { Recipe } from 'src/models/recipe';
-import { RecipeService } from 'src/app/services/recipe-service/recipe.service';
+import { Component, OnInit } from '@angular/core';
 import { MatSnackBar } from '@angular/material/snack-bar';
+import { Router } from '@angular/router';
 import { plainToClass } from 'class-transformer';
-import { SnackBarHelper } from 'src/app/helpers/snack-bar.helper';
-import { catchError, tap, throwError } from 'rxjs';
-import { HttpErrorResponse } from '@angular/common/http';
+import { AnimationHelper } from 'src/app/helpers/animation-helper';
+import { RecipeService } from 'src/app/services/recipe-service/recipe.service';
+import { Recipe } from 'src/models/recipe';
+import { User } from 'src/models/user';
+
+import { AuthService } from './../../services/auth-service/auth.service';
 
 @Component( {
     selector: 'app-user-home',
     templateUrl: './user-home.component.html',
     styleUrls: [ './user-home.component.scss' ],
+    animations: [ AnimationHelper.getSimpleFade( 'slowFade', 400 ) ],
 } )
 export class UserHomeComponent implements OnInit {
     public recipes!: Recipe[];
+    public currentUser!: any;
 
     constructor(
         private recipeService: RecipeService,
+        private authService: AuthService,
         private router: Router,
         private _snackBar: MatSnackBar
     ) {}
@@ -31,9 +34,36 @@ export class UserHomeComponent implements OnInit {
                 this.recipes = this.recipes.reverse();
             }
         );
+
+        this.authService.getCurrentUser().subscribe( ( user: any ) => {
+            this.currentUser = plainToClass( User, user );
+        } );
     }
 
     viewRecipe( recipeId: string | undefined ): void {
         this.router.navigate( [ `/recipes/${recipeId}` ] );
+    }
+
+    getRecentRecipes(): Recipe[] {
+        let recentRecipes: Recipe[];
+
+        if ( this.recipes.length < 3 ) {
+            recentRecipes = this.recipes;
+        } else {
+            recentRecipes = this.recipes.slice(
+                this.recipes.length - 3,
+                this.recipes.length
+            );
+        }
+
+        return recentRecipes;
+    }
+
+    openCreatePage(): void {
+        this.router.navigate( [ '/recipes/create' ] );
+    }
+
+    openUploadPage(): void {
+        this.router.navigate( [ '/recipes/upload' ] );
     }
 }
