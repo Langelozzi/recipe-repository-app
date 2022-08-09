c316c10831ce039379bedb99f505ca25821388d1
Lucas Angelozzi <lucasangelozzi32@gmail.com>
2022-08-08 22:49:28 -0700

    started tag sorting functionality

---
+++ src/models/recipe-batch.ts
@@ -0,0 +1,35 @@
+import { Recipe } from './recipe';
+
+export class RecipeBatch {
+    recipes: Recipe[] = [];
+
+    constructor( recipes: Recipe[] ) {
+        this.recipes = recipes;
+    }
+
+    public getAllTags(): Set<string> {
+        const allTags: Set<string> = new Set();
+
+        for ( const recipe of this.recipes ) {
+            if ( recipe.tags && recipe.tags?.length > 0 ) {
+                for ( let i = 0; i < recipe.tags.length; i++ ) {
+                    allTags.add( recipe.tags[i] );
+                }
+            }
+        }
+
+        return allTags;
+    }
+
+    public getRecipesByTag( tag: string ): Recipe[] {
+        const sortedRecipes: Recipe[] = [];
+        
+        for ( const recipe of this.recipes ) {
+            if ( recipe.tags?.includes( tag ) ) {
+                sortedRecipes.push( recipe );
+            }
+        }
+
+        return sortedRecipes;
+    }
+}
